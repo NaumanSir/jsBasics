009f77ffbc6df8be2ea44b4a65578088f3de4de5
Nauman Husnani <naumanhusnani@gmail.com>
2019-02-26 10:03:18 -0600

    new tip calculator

--- script.js
+++ script.js
@@ -475,3 +475,31 @@ var john = ['John', 'Smith', 1990, 'teacher', false];
 for (var i=john.length-1; i>=0; i--) {
     console.log(john[i]);
 }
+
+
+/*************************************************
+ * Coding Challenge 5
+ */
+
+var john= {
+    name: 'John',
+    bills: [124, 48, 268, 180, 42],
+    calcTips: function() {
+        this.tips = [];
+        this.finalCheck = [];
+        for(var i=0; i<this.bills.length, i++) {
+            var percentage;
+            var bill = this.bills[i];
+
+            if(bill < 50) {
+                percentage = .2;
+            } else if (bill >= 50 && bill < 200) {
+                percentage = .15;
+            } else {
+                percentage = .1;
+            }
+            this.tips[i] = bill * percentage;
+            this.finalCheck[i] = bill + bill * percentage;
+        }
+    }
+}
